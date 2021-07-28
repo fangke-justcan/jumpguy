e8efb38aa79dd66baa4fec58e576d80788093618
fangke <fang.ke@sz.tsinghua.edu.cn>
2021-07-28 16:13:47 +0800

    AI 7-28-2

--- Assets/ObstacleCoursePack/Scripts/BananaMan.cs
+++ Assets/ObstacleCoursePack/Scripts/BananaMan.cs
@@ -7,10 +7,6 @@ public class BananaMan : MonoBehaviour
 {
     GameObject ballInHand;
     int currentTarget;
-    NavMeshAgent agent;
-    //public float speed;
-    // Start is called before the first frame update
-
     public Transform rightHand;
     public Transform player;
     public GameObject ball;
@@ -40,62 +36,12 @@ public class BananaMan : MonoBehaviour
 
     void Start()
     {
-        /*
-        agent = GetComponent<NavMeshAgent>();
-        currentTarget = 0;
-        isFollowingPlayer = false;
-        agent.SetDestination(wps[currentTarget].transform.position);
-        */
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+       
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        /*
-        Vector3 dectectStart = new Vector3(transform.position.x, playerAIDetection.transform.position.y, transform.position.z);
-
-        
-        Debug.DrawLine(dectectStart, dectectStart + transform.forward.normalized*10, Color.red);
-        RaycastHit hit;
-        Ray ray = new Ray(dectectStart, transform.forward);
-        if (Physics.Raycast(ray,out hit, 10))
-        {
-            if(hit.collider.tag == "AIDetection")
-            {
-                isFollowingPlayer = true;
-                Debug.Log("Following Player");
-                
-            }
-        }
-        if (isFollowingPlayer)
-        {
-            agent.SetDestination(playerAIDetection.transform.position);
-            if (Vector3.Distance(dectectStart, playerAIDetection.transform.position)> 10) 
-            {   
-                isFollowingPlayer = false; 
-                agent.SetDestination(wps[currentTarget].transform.position); 
-                Debug.Log("NO Following"); 
-            }
-
-        }
-        else
-        {
-            if (wps.Length == 0) return;
-            if (agent.remainingDistance < 1)
-            {
-                currentTarget++;
-                if (currentTarget == wps.Length) currentTarget = 0;
-                agent.SetDestination(wps[currentTarget].transform.position);
-            }
-
-            
-
-
-        }
-       
-
-        */
+   
        
         
-    }
+    
 }
