9880f3d773c18d2457e0e98c05f3e3daaa80b095
fangke <fang.ke@sz.tsinghua.edu.cn>
2021-07-28 11:08:52 +0800

    AI 7-28

--- Assets/ObstacleCoursePack/Scripts/BananaMan.cs
+++ Assets/ObstacleCoursePack/Scripts/BananaMan.cs
@@ -5,24 +5,53 @@ using UnityEngine.AI;
 
 public class BananaMan : MonoBehaviour
 {
-    bool isFollowingPlayer;
-    public GameObject[] wps;
-    public GameObject playerAIDetection;
+    GameObject ballInHand;
     int currentTarget;
     NavMeshAgent agent;
     //public float speed;
     // Start is called before the first frame update
+
+    public Transform rightHand;
+    public Transform player;
+    public GameObject ball;
+    public void MakingBall()
+    {
+        GameObject clone;
+        clone = Instantiate(ball, rightHand);
+        ballInHand = clone;
+        Object.Destroy(clone, 3);
+        
+ 
+    }
+
+    public void ThrowingBall()
+    {
+        
+        ballInHand.AddComponent<Rigidbody>();
+        Vector3 dir = (player.position - ballInHand.transform.position).normalized; 
+        ballInHand.GetComponent<Rigidbody>().AddForce(dir * 2000);
+        ballInHand.GetComponent<Rigidbody>().mass = 10000;
+
+        ballInHand.transform.SetParent(null);
+
+
+    }
+
+
     void Start()
     {
+        /*
         agent = GetComponent<NavMeshAgent>();
         currentTarget = 0;
         isFollowingPlayer = false;
         agent.SetDestination(wps[currentTarget].transform.position);
+        */
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        /*
         Vector3 dectectStart = new Vector3(transform.position.x, playerAIDetection.transform.position.y, transform.position.z);
 
         
@@ -65,7 +94,7 @@ public class BananaMan : MonoBehaviour
         }
        
 
-
+        */
        
         
     }
